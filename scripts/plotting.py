f52fdf95e2fa383b7f272a80ce9661458eed235d
Esther <ecai00@berkeley.edu>
2023-12-07 12:49:17 -0800

    upadte plotting

--- scripts/plotting.py
+++ scripts/plotting.py
@@ -2,21 +2,26 @@ import matplotlib.pyplot as plt
 import re
 
 # File path - update this to the path of your file
-file_path = r"C:\Users\yuche\Documents\image_to_depthmap\scripts\loss_epoch170.txt"
+file_path = r"C:\Users\yuche\Downloads\loss_log_3.txt"
 
-# Initialize a dictionary to store the losses
-losses = {'G_GAN': []}
+# Initialize a dictionary to store the losses and a list for iteration numbers
+losses = {'G_L1': []}
+iterations = []
 
 # Read and parse the data from the file
 with open(file_path, 'r') as file:
     for line in file:
         if line.strip():
+            # Extracting iteration number
+            match = re.search(r'iters: (\d+)', line)
+            if match:
+                iterations.append(int(match.group(1)))
+
             # Splitting the line at parentheses and taking the second part
             loss_part = line.split(')')[1] if ')' in line else line
 
             # Extracting each loss value
             parts = re.findall(r'([A-Za-z_\d]+): ([\d]+[.][\d]+)', loss_part)
-            print(parts)
             for key, value in parts:
                 if key in losses:
                     losses[key].append(float(value))
@@ -24,10 +29,10 @@ with open(file_path, 'r') as file:
 # Plotting
 plt.figure(figsize=(10, 6))
 for key, values in losses.items():
-    plt.plot(values, label=key)
+    plt.plot(iterations, values, label=key)
 
 plt.xlabel('Iterations')
 plt.ylabel('Loss')
-plt.title('Training Loss Over Iterations')
+plt.title('Eval Loss Over Iterations')
 plt.legend()
 plt.show()
